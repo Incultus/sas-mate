2eb9397daaf8b6824779caae51d04e927b16ef54
hatsunearu <dongimin2@gmail.com>
2012-09-23 18:47:17 +0800

    Test commit

--- src/SerialInterface.java
+++ src/SerialInterface.java
@@ -1,7 +1,7 @@
 
 public interface SerialInterface {
 	//this is a test interface.
-	public void connect();
+	public int connect();
 	public void analogWrite(int pinId);
 	public void digitalWrite(int pinId);
 	public double analogRead(int pinId);
