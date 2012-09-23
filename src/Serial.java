0010f0471f343966f74d1bf157873fed2f8bc2a4
hatsunearu <dongimin2@gmail.com>
2012-09-23 19:41:44 +0800

    Sets up Serial external Jars
    RXTXcomm.jar has been added.
    Seral.class, a class that will detail serial connection, has been added

---
+++ src/Serial.java
@@ -0,0 +1,20 @@
+import gnu.io.SerialPortEvent;
+import gnu.io.SerialPortEventListener;
+
+import java.awt.Color;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.TooManyListenersException;
+
+public class Serial implements SerialPortEventListener {
+
+	@Override
+	public void serialEvent(SerialPortEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
