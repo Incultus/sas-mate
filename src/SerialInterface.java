877c779d2edee91cee1a5e8665d6c4360dc72e3f
hatsunearu <dongimin2@gmail.com>
2012-09-23 18:21:02 +0800

    Java directory Setup
    This commit sets up the java directory so it can be read directly from
    Eclipse.

---
+++ src/SerialInterface.java
@@ -0,0 +1,9 @@
+
+public interface SerialInterface {
+	//this is a test interface.
+	public void connect();
+	public void analogWrite(int pinId);
+	public void digitalWrite(int pinId);
+	public double analogRead(int pinId);
+	public boolean digitalRead(int pinId);
+}
